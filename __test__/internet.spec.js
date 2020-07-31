8cb7f64d78fba46973e2754a021f848ddcfd717f
Martins Obayomi <obayomimartins96@gmail.com>
2020-07-31 02:13:41 +0100

    unit tests

--- __test__/internet.spec.js
+++ __test__/internet.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, getByTestId } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { act } from '@testing-library/react-hooks';
 import '@testing-library/jest-dom';
 import InternetStatus from '../src/InternetStatus';
